0dba903ab01ea8a4ef71668f6f946554bc2f69bd
Phuoc Nguyen <me@phuoc.ng>
2020-02-12 00:51:09 +0700

    Fix abn test cases

--- src/abn/checkDigits.ts
+++ src/abn/checkDigits.ts
@@ -10,7 +10,7 @@ const checkDigits = (input: number[]): number => {
     const weight = [3, 5, 7, 9, 11, 13, 15, 17, 19];
     const sum = input.map((v, i) => v * weight[i]).reduce((a, b) => a + b, 0);
 
-    return 11 + (sum - 1) % 89;
+    return (Math.floor(sum / 89) + 1) * 89 + 10 - sum;
 };
 
 export default checkDigits;
--- test/abn.spec.ts
+++ test/abn.spec.ts
@@ -18,20 +18,40 @@ describe('ABN number', () => {
     });
 
     it('Invalid ABN numbers', () => {
-        expect(abn.check('7558041236').valid).to.be.false;  // Invalid length
-        expect(abn.check('A4522250218').valid).to.be.false; // Has invalid character (A)
-        expect(abn.check('16752104342').valid).to.be.false; // Invalid check digits
+        [
+            '705481783',    // Invalid length
+            'A5051470610',  // Has invalid character (A)
+            '15564594618',  // Invalid check digits
+        ].forEach((n) => expect(abn.check(n).valid).to.be.false);
     });
 
     it('Valid ABN numbers', () => {
-        expect(abn.check('75580412363').valid).to.be.true;
-        expect(abn.check('54522250218').valid).to.be.true;
-        expect(abn.check('15752104342').valid).to.be.true;
+        [
+            '83914571673',
+            '51824753556',
+            '70548178358',
+            '25051470610',
+            '16564594618',
+        ].forEach((n) => expect(abn.check(n).valid).to.be.true);
     });
 
     it('Valid ABN number with spaces', () => {
-        expect(abn.check('75 580 412 363').valid).to.be.true;
-        expect(abn.check('54  522  250  218').valid).to.be.true;
-        expect(abn.check('15 752   104  342').valid).to.be.true;
+        [
+            '70 548 178 358',
+            '25   051   470   610',
+            '16  564   594    618',
+            '28 043 145 470',
+            '65 497 794 289',
+            '46 527 394 509',
+            '99 070 045 359',
+            '98 860 905 153',
+            '53 106 288 699',
+            '51 008 129 511',
+            '43 500 713 236',
+            '72 342 387 170',
+            '21 188 299 895',
+            '55 914 901 347',
+            '92 638 328 368',
+        ].forEach((n) => expect(abn.check(n).valid).to.be.true);
     });
 });
