b72a9b99a860dd9a1b2cbb07b27aa65cf2f8ef93
Phuoc Nguyen <me@phuoc.ng>
2020-02-12 21:48:32 +0700

    Fix SVNR generator

--- src/svnr/check.ts
+++ src/svnr/check.ts
@@ -7,6 +7,7 @@
  */
 
 import CheckResult from '../CheckResult';
+import checkDigit from './checkDigit';
 
 const check = (input: string): CheckResult<{}> => {
     const v = input.replace(/\s/g, '');
@@ -14,22 +15,11 @@ const check = (input: string): CheckResult<{}> => {
         return { valid: false };
     }
 
-    const weight = [3, 7, 9, 5, 8, 4, 2, 1, 6];
-    const getCheckDigit = (str: string) => {
-        return str.split('').map((c, i) => parseInt(c, 10) * weight[i]).reduce((a, b) => a + b, 0) % 11;
-    };
+    const serial = parseInt(v.substr(0, 3), 10);
+    const dob = v.substr(4);
+    const cd = checkDigit(serial, dob);
 
-    let sequence = parseInt(v.substring(0, 3), 10);
-    const cd = parseInt(v.substring(3, 4), 10);
-    const dob = v.substring(4);
-
-    let checkDigit = getCheckDigit(`${sequence}${dob}`);
-    while (checkDigit === 10) {
-        sequence += 1;
-        checkDigit = getCheckDigit(`${sequence}${dob}`);
-    }
-
-    return { valid: checkDigit === cd };
+    return { valid: `${cd.checkDigit}` === v.substr(3, 1) };
 };
 
 export default check;
--- test/svnr.spec.ts
+++ test/svnr.spec.ts
@@ -10,10 +10,22 @@ import { expect } from 'chai';
 
 import { svnr } from '../src';
 
-describe('Generate SVNR number', () => {
-    it('SVNR number', () => {
+describe('SVNR number', () => {
+    it('Fake SVNR number', () => {
         const number = svnr.fake();
         const result = svnr.check(number);
         expect(result.valid).to.be.true;
+    });
+
+    it('Valid SVNR numbers', () => {
+        [
+            '1237 010180', '4298021282', '5659081577', '8852041693', '5497042152',
+        ].forEach((n) => expect(svnr.check(n).valid).to.be.true);
+    });
+
+    it('Invalid SVNR numbers', () => {
+        [
+            '4298001282', '1508-10-13-50', '1508101051', '4290021282', '21 34 23 12 74',
+        ].forEach((n) => expect(svnr.check(n).valid).to.be.false);
     });
 });
