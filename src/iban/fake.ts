28de8fd4f1f3d447c2162c12a0a08153b26c0866
Phuoc Nguyen <me@phuoc.ng>
2020-03-01 14:33:05 +0700

    Add iban validator

---
+++ src/iban/fake.ts
@@ -0,0 +1,18 @@
+/**
+ * Generate fake and valid numbers
+ *
+ * @see https://fakenumbers.io
+ * @license https://fakenumbers.io/license
+ * @copyright 2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+/**
+ * Generate a random IBAN
+ *
+ * @returns string
+ */
+const fake = (): string => {
+    return '';
+};
+
+export default fake;
